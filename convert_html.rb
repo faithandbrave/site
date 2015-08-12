4882b4d10b47e3f775b10198cb77581ea5efa17b
Akira Takahashi <faithandbrave@gmail.com>
2015-08-13 04:39:05 +0900

    ページタイトルを付けた

--- convert_html.rb
+++ convert_html.rb
@@ -1,3 +1,21 @@
-system('pandoc --self-contained -o ../website/index.html index.md -f markdown+hard_line_breaks --template=template.html -c github-markdown.css')
+def set_page_title(src_data, dst_data)
+  site_name = 'Faith and Brave'
+  front_line = src_data.lines.first.chomp
+  title = front_line.gsub(/#(.*?)/, '\1')
+  return dst_data.gsub('META_TITLE', "#{title} - #{site_name}")
+end
 
+src_filename = 'index.md'
+dst_filename = '../website/index.html'
+
+system("pandoc --self-contained -o #{dst_filename} #{src_filename} -f markdown+hard_line_breaks --template=template.html -c github-markdown.css")
+
+src_data = File.open(src_filename).read
+dst_data = File.open(dst_filename).read
+
+dst_data = set_page_title(src_data, dst_data)
+
+File.open(dst_filename, 'w') {|f|
+  f.write(dst_data)
+}
 
