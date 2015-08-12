2f84e16140a6b329548fe378bd327e2fe9eb3d11
Akira Takahashi <faithandbrave@gmail.com>
2015-08-13 04:15:24 +0900

    雛形ファイルを使用するようにした(fix #1)

--- convert_html.rb
+++ convert_html.rb
@@ -1,3 +1,3 @@
-system('pandoc --self-contained -o ../website/index.html index.md -f markdown+hard_line_breaks -c github-markdown.css')
+system('pandoc --self-contained -o ../website/index.html index.md -f markdown+hard_line_breaks --template=template.html -c github-markdown.css')
 
 
