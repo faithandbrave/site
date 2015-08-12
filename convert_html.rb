424651601679f9ea186fb25885f8e98c86978406
Akira Takahashi <faithandbrave@gmail.com>
2015-08-13 04:44:56 +0900

    リファクタ

--- convert_html.rb
+++ convert_html.rb
@@ -5,17 +5,23 @@ def set_page_title(src_data, dst_data)
   return dst_data.gsub('META_TITLE', "#{title} - #{site_name}")
 end
 
+def convert_after(src_filename, dst_filename)
+  src_data = File.open(src_filename).read
+  dst_data = File.open(dst_filename).read
+
+  dst_data = set_page_title(src_data, dst_data)
+
+  File.open(dst_filename, 'w') {|f|
+    f.write(dst_data)
+  }
+end
+
+def convert(src_filename, dst_filename)
+  system("pandoc --self-contained -o #{dst_filename} #{src_filename} -f markdown+hard_line_breaks --template=template.html -c github-markdown.css")
+  convert_after(src_filename, dst_filename)
+end
+
 src_filename = 'index.md'
 dst_filename = '../website/index.html'
-
-system("pandoc --self-contained -o #{dst_filename} #{src_filename} -f markdown+hard_line_breaks --template=template.html -c github-markdown.css")
-
-src_data = File.open(src_filename).read
-dst_data = File.open(dst_filename).read
-
-dst_data = set_page_title(src_data, dst_data)
-
-File.open(dst_filename, 'w') {|f|
-  f.write(dst_data)
-}
+convert(src_filename, dst_filename)
 
