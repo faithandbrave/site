aa499e40dbd9e85add6a34b77eb73efc1feba298
Akira Takahashi <faithandbrave@gmail.com>
2015-08-18 20:14:17 +0900

    プロフィールページを追加

--- commit.rb
+++ commit.rb
@@ -1,9 +1,11 @@
 system('git add -A')
 system("git commit -a -m \"#{ARGV[0]}\"")
+system('git push')
 
 Dir.chdir('../website')
 system('git add -A')
 system('git commit -a -m "converted"')
+system('git push')
 
 Dir.chdir('../site')
 
