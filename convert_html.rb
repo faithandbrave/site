4d5b3ecb7b359e4152b546be260d7ec258d21a84
Akira Takahashi <faithandbrave@gmail.com>
2015-08-18 20:02:06 +0900

    Revert "プロフィールページを追加"
    This reverts commit 1f524634dcbc73ad7f496774df1aa7383ceaea24.

--- convert_html.rb
+++ convert_html.rb
@@ -40,15 +40,9 @@ def convert(src_filename, dst_filename)
   convert_after(src_filename, dst_filename)
 end
 
-def replace_extension(md_filename)
-  return md_filename.gsub(/(.*?)\.md/, '\1.html')
-end
-
-target_file_list = ['index.md', 'profile.md']
-target_file_list.each {|filename|
-  dest_directory = '../website/'
-  convert(filename, replace_extension(dest_directory + filename))
-}
+src_filename = 'index.md'
+dst_filename = '../website/index.html'
+convert(src_filename, dst_filename)
 
 FileUtils.copy('theme.css', '../website/theme.css')
 
